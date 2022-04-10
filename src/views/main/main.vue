0bb07b06b84a359700dc25fa98c8c5ae48a6820c
yuchengtian <yuchengtian@gmail.com>
2022-04-10 19:18:45 +0800

    ci: 搭建项目基础功能

---
+++ src/views/main/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Main</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'MainComp',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
